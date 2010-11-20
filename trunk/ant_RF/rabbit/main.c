4241d8baa54d214ac42215f9946cfa0a58022ad1
edgardovaz <edgardovaz@948cf517-5fb8-8a2b-d655-638daefefee0>
2010-11-20 23:10:56 +0000

    Agrego la librería utils.lib, donde se definen funciones generales como printHexa() la cual imprime arreglos de bytes en formato hexadecimal. En main.c se llama a esta función para imprimir los arreglos de bytes. En la librería de configuración de puertos io_config.lib se cambió el valor de SPI_CLOCK del valor 100 al valor por defecto 5.

--- trunk/ant_RF/rabbit/main.c
+++ trunk/ant_RF/rabbit/main.c
@@ -5,17 +5,16 @@
 #class auto
 #use "io_config.lib"
 #use "rc632.lib"
-
-
+#use "utils.lib"
 
 void isr_RF();
 
 void main()
 {
-	char read_e2prom[2];
-   unsigned char aux[4];
-   unsigned char cmd[2];
-   unsigned char data_e2prom[16];
+	byte read_e2prom[2];
+   byte aux[4];
+   byte cmd[2];
+   byte data_e2prom[16];
    unsigned int i;
    word t;
 
@@ -31,7 +30,7 @@ void main()
    BitWrPortI(PEDR, &PEDRShadow, 0, 1);	// chip select low
    BitWrPortI(PCDR, &PCDRShadow, 1, 4);	// not reset
 
-   aux[0] = 0x04;
+   aux[0] = 0x04;     //00000100 escribir en el buffer FIFO
    aux[1] = 0x00;    //LSB
    aux[2] = 0x0F;    //MSB
    aux[3] = 0x10;    //Nº de bytes
@@ -49,13 +48,13 @@ void main()
 
 	SPIRead(data_e2prom, 16);
 
+   printf("Valor del arreglo auxiliar:\n");
+	printHexa(aux, 4);
+
    printf("Valor de la posicion 0 de la E2PROM:\n");
-   for(i=0; i<16; i++)
-   {
-   	printf("%02X", data_e2prom[i]);
-   }
+	printHexa(data_e2prom, 16);
 
-
+   while(1);
 }
 
 
