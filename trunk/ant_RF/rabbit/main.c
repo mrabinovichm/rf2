1a48797e2700fa2ab93dfd8611244a1507d4d662
edgardovaz <edgardovaz@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-04-03 16:38:29 +0000

    La secuencia de activación de una tarjeta está ahora completamente modularizada. En el main se llama directamente a la nueva función iso14443a_activ_secuence() que lleva adelante el algoritmo de anticolisión.

--- trunk/ant_RF/rabbit/main.c
+++ trunk/ant_RF/rabbit/main.c
@@ -14,8 +14,8 @@
 
 void main()
 {
-	byte status, bcc, checksum, ac_sel_code_CL;
-   byte atq[2], atq_len[1], CtUidBcc[5], CtUidBcc_len[1], sak[3], sak_len[1];
+	byte status, bcc, checksum, ac_sel_code_CL, UID_MF_len;
+   byte atq[2], atq_len[1], UID_MF[10], sak[3], sak_len[1];
    short complete, level;
    auto word t;
 
@@ -36,44 +36,14 @@ void main()
    t = _SET_SHORT_TIMEOUT(10);        /*espera de 10ms*/
 	while(!_CHK_SHORT_TIMEOUT(t));
 
-   status = iso14443a_request(ISO14443A_SF_CMD_REQA, atq, atq_len);
+	status = iso14443a_activ_secuence(ISO14443A_SF_CMD_REQA, atq, UID_MF, &UID_MF_len, sak);
    printf("STATUS: %02X\n", status);
-   printf("Respuesta de la tarjeta: ");
-	printHexa(atq, atq_len[0]);
-   if(status == 0x00 && atq[0] == 0x04)
-   {
-   	level = ISO14443A_LEVEL_CL1;
-   	complete = 0;
-   	while(!complete)
-   	{
-      	switch(level)
-         {
-         	case ISO14443A_LEVEL_CL1: ac_sel_code_CL = ISO14443A_AC_SEL_CODE_CL1; break;
-            case ISO14443A_LEVEL_CL2: ac_sel_code_CL = ISO14443A_AC_SEL_CODE_CL2; break;
-            case ISO14443A_LEVEL_CL3: ac_sel_code_CL = ISO14443A_AC_SEL_CODE_CL3; break;
-            default: printf("Error Cascade Level\n");
-         }
-   		status = iso14443a_anticollition(ac_sel_code_CL, CtUidBcc, CtUidBcc_len, &checksum);
- printf("STATUS: %02X\n", status);
- printf("Respuesta de la tarjeta: ");
- printHexa(CtUidBcc, CtUidBcc_len[0]);
- printf("Checksum OK: %02X\n", checksum);
-   		bcc = CtUidBcc[4];
-   		if(bcc == checksum)
-         {
-				status = iso14443a_select(ac_sel_code_CL, CtUidBcc, sak, sak_len);
-printf("STATUS: %02X\n", status);
-printf("Respuesta de la tarjeta: ");
-printHexa(sak, sak_len[0]);
-   			if(CtUidBcc[0] != CT)
-            	complete = 1;
-     			else
-         		level++;
-         }
-         else
-         	printf("Error Checksum\n");
-      }
-   }
+   printf("ATQ de la tarjeta: ");
+	printHexa(atq, ATQ_SIZE);
+	printf("UID de la tarjeta: ");
+	printHexa(UID_MF, UID_MF_len);
+   printf("SAK de la tarjeta: ");
+	printHexa(sak, SAK_SIZE);
 
    t = _SET_SHORT_TIMEOUT(2000);        /*espera de 2s*/
 	while(!_CHK_SHORT_TIMEOUT(t));
