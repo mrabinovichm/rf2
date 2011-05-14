71ae6786b865aea354a44cac020e8827ed220668
edgardovaz@gmail.com <edgardovaz@gmail.com@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-05-14 14:59:48 +0000

    Librería para el manejo de los pines gpio.

---
+++ gpio/gpio.c
@@ -0,0 +1,82 @@
+#include "gpio.h"
+
+FILE *fp;
+
+int config_gpio_pin(char *dir, char *pinN)
+{
+	char gpio_path[30];
+	char len_dir = strlen(dir);
+	char len_pinN = strlen(pinN);
+	
+	strcpy(gpio_path, "/sys/class/gpio/gpio");
+	strcat(gpio_path, pinN); 
+	strcat(gpio_path, "/direction");
+	
+	if ((fp = fopen("/sys/class/gpio/export", "ab")) == NULL)
+		return -1;
+	rewind(fp);	/*Set pointer to begining of the file*/
+	fwrite(pinN, sizeof(char), len_pinN, fp); /*Write number pin to the file*/
+	fclose(fp);	
+	
+	if ((fp = fopen(gpio_path, "rb+")) == NULL) /*file in binary for reading and writing*/
+		return -1;
+	rewind(fp);	/*Set pointer to begining of the file*/
+	fwrite(dir, sizeof(char), len_dir, fp); /*Write "out" or "in" to the file*/
+	fclose(fp);
+	
+	return 0;		
+}
+	
+int read_gpio_pin(char *pinN)
+{
+	char gpio_value;
+	char gpio_path[30];
+	strcpy(gpio_path, "/sys/class/gpio/gpio");
+	strcat(gpio_path, pinN); 
+	strcat(gpio_path, "/value");
+		
+	if ((fp = fopen(gpio_path, "rb")) == NULL) /*file in binary for reading only*/
+		return -1;
+		
+	rewind(fp);	/*Set pointer to begining of the file*/
+	fread(&gpio_value, sizeof(char), 1, fp); /*Value read of the file*/
+	fclose(fp);
+	
+	printf("Valor del pin %s: %c\n", pinN, gpio_value);
+	
+	return 0;	
+}
+	
+int set_gpio_pin(char *pinN)
+{
+	char gpio_path[30];
+	strcpy(gpio_path, "/sys/class/gpio/gpio");
+	strcat(gpio_path, pinN); 
+	strcat(gpio_path, "/value");
+		
+	if ((fp = fopen(gpio_path, "rb+")) == NULL) /*file in binary for reading and writing*/
+		return -1;
+		
+	rewind(fp);	/*Set pointer to begining of the file*/
+	fwrite("1", sizeof(char), 1, fp); /*Write "1" to the file*/
+	fclose(fp);
+	
+	return 0;
+}
+	
+int clear_gpio_pin(char *pinN)
+{
+	char gpio_path[30];
+	strcpy(gpio_path, "/sys/class/gpio/gpio");
+	strcat(gpio_path, pinN); 
+	strcat(gpio_path, "/value");
+		
+	if ((fp = fopen(gpio_path, "rb+")) == NULL) /*file in binary for reading and writing*/
+		return -1;
+		
+	rewind(fp);	/*Set pointer to begining of the file*/
+	fwrite("0", sizeof(char), 1, fp); /*Write "0" to the file*/
+	fclose(fp);	
+	
+	return 0;
+}	
