53df0d28ef2f281f072c7211d12f7ae2522d8663
edgardovaz@gmail.com <edgardovaz@gmail.com@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-05-15 00:51:41 +0000

    Se agrega verificación para no efectuar un set o clean en un pin de entrada. Se define una estructura donde se guarda el estado de cada pin. Hay un problema, al llamar a la función de configuración se debe repetir la llamda para el primer pin que se vaya a configurar porque no queda almacenada la dirección del pin en el status del pin.

--- trunk/gpio/main.c
+++ trunk/gpio/main.c
@@ -1,20 +1,40 @@
 #include "gpio.h"
+#include "beagle_gpio.h"
+
+const char OUT[] = "out";
+const char IN[] = "in";
 
 int main(void){
 
-	config_gpio_pin(OUT, "144");
-	config_gpio_pin(OUT, "168");
+	status_gpio status_pin4;
+	status_gpio status_pin13;
+	status_gpio status_pin24;
 
-	read_gpio_pin("144");
-	read_gpio_pin("168");
+status_pin4.direction = '0';
+printf("Dirección del pin %s: %c\n", PIN4, status_pin4.direction);
+
+	config_gpio_pin(&status_pin4, OUT, PIN4);
+	config_gpio_pin(&status_pin13, IN, PIN13);
+	config_gpio_pin(&status_pin24, OUT, PIN24);
+//config_gpio_pin(&status_pin4, OUT, PIN4);
+
+	read_gpio_pin(&status_pin4, PIN4);
+	read_gpio_pin(&status_pin24, PIN24);
+	
+	printf("Dirección del pin %s: %c\n", PIN4, status_pin4.direction);
+	printf("Dirección del pin %s: %c\n", PIN13, status_pin13.direction);
+	printf("Dirección del pin %s: %c\n", PIN24, status_pin24.direction);
+	
+	set_gpio_pin(&status_pin13, PIN13);
+	clear_gpio_pin(&status_pin13, PIN13);
 
 	while(1)
 	{
-		set_gpio_pin("144");
-		clear_gpio_pin("168");
+		set_gpio_pin(&status_pin4, PIN4);
+		clear_gpio_pin(&status_pin24, PIN24);	
 		sleep(1);
-		set_gpio_pin("168");
-		clear_gpio_pin("144");
+		set_gpio_pin(&status_pin24, PIN24);
+		clear_gpio_pin(&status_pin4, PIN4);		
 		sleep(1);
 	}
 	return 0;
