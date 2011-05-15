c47e00a143f1bcac4ef1e291299341a5024ed8d6
edgardovaz@gmail.com <edgardovaz@gmail.com@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-05-15 00:53:23 +0000

    Agrego lista de pines del conector de expansión de la Beagleboard

---
+++ trunk/gpio/beagle_gpio.h
@@ -0,0 +1,24 @@
+#ifndef BEAGLE_GPIO_H
+#define BEAGLE_GPIO_H
+
+/*EXPANSION CONNECTOR*/
+/*Pines usados como GPIO*/
+
+#define PIN3  "139"
+#define PIN4  "144"
+#define PIN5  "138"
+#define PIN7  "137"
+#define PIN9  "136"
+#define PIN10 "145"
+#define PIN12 "158"
+#define PIN13 "134"
+#define PIN14 "162"
+#define PIN15 "133"
+#define PIN16 "161"
+#define PIN18 "159"
+#define PIN20 "156"
+#define PIN22 "157"
+#define PIN23 "183"
+#define PIN24 "168"
+
+#endif /*BEAGLE_GPIO_H*/
