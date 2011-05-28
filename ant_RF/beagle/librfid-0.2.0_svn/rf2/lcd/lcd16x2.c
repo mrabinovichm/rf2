c52b615f1bd4903c3ba17f7938a5f4f3b0e82392
daicav <daicav@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-05-28 22:05:06 +0000

    Se agrega directorio lcd para el manejo del display dentro de la lbreria

---
+++ ant_RF/beagle/librfid-0.2.0_svn/rf2/lcd/lcd16x2.c
@@ -0,0 +1,299 @@
+#include "../gpio/beagle_gpio.h"
+#include "../gpio/gpio.h"
+#include "lcd16x2.h"
+
+status_gpio status_D7;
+status_gpio status_D6;
+status_gpio status_D5;
+status_gpio status_D4;
+status_gpio status_RW;
+status_gpio status_RS;
+status_gpio status_BL;
+status_gpio status_E;
+status_gpio status_LV;
+status_gpio status_LR;
+status_gpio status_LA;
+
+
+
+/* ******************************************************************************************************* */
+/* ******************************** Inicialización de pines en beagleboard *********************************/
+/* ******************************************************************************************************* */
+
+void init_gpio(void)
+{
+	config_gpio_pin(&status_D7, OUT, PIN9);			/*D7*/
+	config_gpio_pin(&status_D6, OUT, PIN16);		/*D6*/
+	config_gpio_pin(&status_D5, OUT, PIN13);		/*D5*/
+	config_gpio_pin(&status_D4, OUT, PIN18);		/*D4*/
+	config_gpio_pin(&status_RW, OUT, PIN20);		/*RW*/
+	config_gpio_pin(&status_RS, OUT, PIN22);		/*RS*/
+	config_gpio_pin(&status_BL, OUT, PIN14);		/*BL*/
+	config_gpio_pin(&status_E, OUT, PIN15);			/*E*/
+		
+}
+/* ******************************************************************************************************* */
+
+
+/* ******************************************************************************************************* */
+/* *********************** Escribe un cartacter en la memoria del display ******************************** */
+/* ******************************************************************************************************* */
+
+void write_lcd(unsigned char simbolo, short ctrl_dat)
+{
+	int i = 0;
+
+	unsigned char aux = simbolo;
+	
+	set_gpio_pin(&status_E, PIN15); 	/* seteo E para luego poder ver la bajada */
+	set_gpio_pin(&status_BL, PIN14);
+
+	if ( (simbolo == FSE) | (simbolo == FSET) )
+	{
+		for (i = 0; i < 4; i++)
+		{
+			simbolo = simbolo << i;
+			simbolo &= 0x80;
+			if (simbolo)
+			{
+				switch (i)
+				{
+					case 0:		set_gpio_pin(&status_D7, PIN9);
+								break;
+					case 1:		set_gpio_pin(&status_D6, PIN16);
+								break;
+					case 2:		set_gpio_pin(&status_D5, PIN13);
+								break;
+					case 3:		set_gpio_pin(&status_D4, PIN18);
+								break;
+					case 4:		set_gpio_pin(&status_D7, PIN9);
+								break;
+					case 5:		set_gpio_pin(&status_D6, PIN16);
+								break;
+					case 6:		set_gpio_pin(&status_D5, PIN13);
+								break;
+					case 7:		set_gpio_pin(&status_D4, PIN18);
+								break;
+					default: 	break;
+				}
+			} else 
+			{
+				switch (i)
+				{
+					case 0:		clear_gpio_pin(&status_D7, PIN9);
+								break;
+					case 1:		clear_gpio_pin(&status_D6, PIN16);
+								break;
+					case 2:		clear_gpio_pin(&status_D5, PIN13);
+								break;
+					case 3:		clear_gpio_pin(&status_D4, PIN18);
+								break;
+					case 4:		clear_gpio_pin(&status_D7, PIN9);
+								break;
+					case 5:		clear_gpio_pin(&status_D6, PIN16);
+								break;
+					case 6:		clear_gpio_pin(&status_D5, PIN13);
+								break;
+					case 7:		clear_gpio_pin(&status_D4, PIN18);
+								break;
+					default: 	break;
+				}			
+			}
+			
+						
+			if (i == 3)
+			{
+				clear_gpio_pin(&status_RS, PIN22);
+				clear_gpio_pin(&status_RW, PIN20);
+				
+				clear_gpio_pin(&status_E, PIN15);
+				set_gpio_pin(&status_E, PIN15);
+			}
+			
+			simbolo = aux;
+		}
+	} else 
+	{
+			for (i = 0; i < 8; i++)
+			{
+				simbolo = simbolo << i;
+				simbolo &= 0x80;
+				if (simbolo)
+				{
+					switch (i)
+					{
+						case 0:		set_gpio_pin(&status_D7, PIN9);
+									break;
+						case 1:		set_gpio_pin(&status_D6, PIN16);
+									break;
+						case 2:		set_gpio_pin(&status_D5, PIN13);
+									break;
+						case 3:		set_gpio_pin(&status_D4, PIN18);
+									break;
+						case 4:		set_gpio_pin(&status_D7, PIN9);
+									break;
+						case 5:		set_gpio_pin(&status_D6, PIN16);
+									break;
+						case 6:		set_gpio_pin(&status_D5, PIN13);
+									break;
+						case 7:		set_gpio_pin(&status_D4, PIN18);
+									break;
+						default: 	break;
+					}
+			} else 
+			{
+				switch (i)
+				{
+					case 0:		clear_gpio_pin(&status_D7, PIN9);
+								break;
+					case 1:		clear_gpio_pin(&status_D6, PIN16);
+								break;
+					case 2:		clear_gpio_pin(&status_D5, PIN13);
+								break;
+					case 3:		clear_gpio_pin(&status_D4, PIN18);
+								break;
+					case 4:		clear_gpio_pin(&status_D7, PIN9);
+								break;
+					case 5:		clear_gpio_pin(&status_D6, PIN16);
+								break;
+					case 6:		clear_gpio_pin(&status_D5, PIN13);
+								break;
+					case 7:		clear_gpio_pin(&status_D4, PIN18);
+								break;
+					default: 	break;
+				}
+			}
+		
+			switch (ctrl_dat)
+			{
+				case CTRL_WR:	clear_gpio_pin(&status_RS, PIN22);
+								clear_gpio_pin(&status_RW, PIN20);
+								break;
+				case CTRL_RD:	clear_gpio_pin(&status_RS, PIN22);
+								set_gpio_pin(&status_RW, PIN20);
+								break; 
+				case DATO_WR: 	set_gpio_pin(&status_RS, PIN22);
+								clear_gpio_pin(&status_RW, PIN20);
+								break; 
+				case DATO_RD:	set_gpio_pin(&status_RS, PIN22);
+								set_gpio_pin(&status_RW, PIN20);
+								break; 
+				default:		break;
+			}
+				
+		
+			if ((i == 3) | (i == 7)){
+				clear_gpio_pin(&status_E, PIN15);
+				set_gpio_pin(&status_E, PIN15);
+			}
+				
+			simbolo = aux;
+		}
+	}
+}
+/* ******************************************************************************************************* */
+                                                                         
+																		 
+/* ******************************************************************************************************* */
+/* **************************** Produce un retardo proporcional a 100us ********************************** */
+/* ******************************************************************************************************* */
+
+void delay(int factor)
+{
+	usleep(factor*100);
+}
+/* ******************************************************************************************************* */
+
+
+/* ******************************************************************************************************* */
+/* ********************** Inicializa el display LCD16x2, pag.46 HD44780U.pdf ***************************** */
+/* ******************************************************************************************************* */
+
+void init_lcd(void)
+{
+	delay(150);  						/* esperar 15ms */
+	write_lcd(FSET, CTRL_WR);			/* comando Function set */
+	delay(50);  						/* esperar 5ms */
+	write_lcd(FSET, CTRL_WR);			/* comando Function set */
+	delay(1);  				    		/* esperar 100us */
+	write_lcd(FSET, CTRL_WR);			/* comando Function set */
+	delay(1);  				    		/* esperar 100us */
+	write_lcd(FSE, CTRL_WR);			/* comando Function set */
+	delay(1);  				    		/* esperar 100us */
+	write_lcd(FUN_SET, CTRL_WR);			/* comando Function set */
+	delay(1);  				    		/* esperar 100us */
+	write_lcd(DPLY_OFF, CTRL_WR);	    	/* comando Dply off */
+	delay(1);  				    		/* esperar 100us */
+	write_lcd(CLEAR, CTRL_WR);	    		/* comando Clear Dply */
+	delay(1);  				    	/* esperar 10ms */
+	write_lcd(ETY_MOD_SET, CTRL_WR);	    /* comando Entry mode set */
+	delay(1);							/* esperar 100us */
+	write_lcd(DPLY_ON, CTRL_WR);	    	/* comando Dply on */
+	delay(1);  				    		/* esperar 100us */
+}
+/* ******************************************************************************************************* */                                                                                                                    
+
+
+/* ******************************************************************************************************* */     
+/* ************ Envia los datos que se escribiran en el display y la longitud de los mismos ************** */
+/* ******************************************************************************************************* */
+ 
+void dato_lcd(unsigned char *dato, int len)
+{
+	short i;
+	unsigned char blank = ' ';
+	if(len <= LCD_16)                                                  
+  	{                                                                  
+  		for(i=0; i<len; i++)                                           
+  		{   
+			if (dato[i] == blank)
+			{
+				write_lcd('A', DATO_WR);
+			} else
+			{
+				write_lcd(dato[i], DATO_WR);                               
+			}
+				delay(1);  				    		/* esperar 100us */      	
+  		}                                                              
+  	}                                                                  
+  	else                                                               
+  	{                                                                  
+  		for(i=0; i<LCD_16; i++)                                        
+  		{   
+			if (dato[i] == blank)
+			{
+				write_lcd(0x10, DATO_WR);
+			} else
+			{                                                           
+				write_lcd(dato[i], DATO_WR);                               
+			}
+  			delay(1);  				    		/* esperar 100us */      
+  		}                                                              
+  		write_lcd(SDA_LIN, CTRL_WR);                                   
+  		delay(1);  				    			/* esperar 100us */      
+ 		for(i=LCD_16; i<len; i++)                                      
+  		{        
+			if (dato[i] == blank)
+			{
+				write_lcd(0x10, DATO_WR);
+			} else
+			{
+				write_lcd(dato[i], DATO_WR);                               
+			}
+ 			delay(1);  				    		/* esperar 100us */      
+  		}                                                              
+	}
+}
+/* ******************************************************************************************************* */
+
+
+/* ******************************************************************************************************* */     
+/* ******************************* Borra el display y apaga el backlight ********************************* */
+/* ******************************************************************************************************* */
+void apagar(void)
+{
+	write_lcd(CLEAR, CTRL_WR);
+	write_lcd(DPLY_OFF, CTRL_WR);	    	
+	clear_gpio_pin(&status_BL, PIN14);
+}
+
---
+++ ant_RF/beagle/librfid-0.2.0_svn/rf2/lcd/lcd16x2.h
@@ -0,0 +1,47 @@
+#ifndef LCD16x2_H
+#define LCD16x2_H
+
+#define LCD_16 16	   /*Long de una linea del Dply*/
+
+/* ***************************** Lineas de control RS, R/W, E y Backlight ******************************** */
+#define CTRL_WR 0x4200 /* x10xx01x BL=1(backlight on), RS=0, R/W=0, E=1 */
+#define CTRL_RD 0x4600 /* x10xx11x BL=1(backlight on), RS=0, R/W=1, E=1 */
+#define DATO_WR	0x6200 /* x11xx01x BL=1(backlight on), RS=1, R/W=0, E=1 */
+#define DATO_RD	0x6600 /* x11xx11x BL=1(backlight on), RS=1, R/W=1, E=1 */
+
+/* ********************************* Palabras de control del LCD16x2 ************************************* */
+#define CLEAR	      0x01 /* 00000001 Borra Dply y vuelve cursor al inicio		   */
+#define RET_HOME      0x02 /* 00000010 Contador de direcciones a cero			   */
+#define ETY_MOD_SET	  0x06 /* 00000110 I/D=1 inc DDRAM, S=0 sin salto de Dply	   */
+#define DPLY_ON		  0x0C /* 00001100 D=1 Dply on,  C=0 sin cursor, B=0 sin blink */
+#define DPLY_OFF	  0x08 /* 00001000 D=0 Dply off, C=0 sin cursor, B=0 sin blink */
+#define CSOR_SHFT  	  0x18 /* 00011000 */
+#define FUN_SET		  0x28 /* 00111000 DL=0 4 bits, N=1 2_lineas, F=don't care	   */
+#define SDA_LIN		  0xC0 /* 11000000 Posiciona el cursor en la 2da linea		   */
+
+#define FSET		  0x30 /* 00110000 */
+#define FSE		  	  0x20 /* 00100000 */
+/* ******************************************************************************************************* */
+
+/* Inicializa los GPIO en beagleboard */
+void init_gpio(void);
+
+/* Escribe un cartacter en la memoria del display */
+void write_lcd(unsigned char simbolo, short ctrl_dat);
+
+/* Produce un retardo proporcional a 100us */
+void delay(int factor);
+
+/* Inicializa el display LCD16x2 */
+void init_lcd(void);
+
+/* Envia los datos que se escribiran en el display */
+void dato_lcd(unsigned char *dato, int len);
+
+/* Borra el display y apaga el backlight */
+void apagar(void);
+
+//void ppal(void);
+
+
+#endif /*LCD16x2_H*/
