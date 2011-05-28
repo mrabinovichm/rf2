05627ae90bda9ce912e978eafef8a541f3de8a8c
edgardovaz@gmail.com <edgardovaz@gmail.com@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-05-28 17:04:15 +0000

    Se agregan librerías para el uso dentro de librfid.

---
+++ ant_RF/beagle/librfid-0.2.0_svn/rf2/gpio/gpio.c
@@ -0,0 +1,89 @@
+#include "gpio.h"
+
+FILE *fp;
+
+int config_gpio_pin(status_gpio *status, const char *dir, const char *pinN)
+{
+	char gpio_path[50];
+	char len_dir = strlen(dir);
+	char len_pinN = strlen(pinN);
+
+	strcpy(gpio_path, "/sys/class/gpio/gpio");
+	strcat(gpio_path, pinN); 
+	strcat(gpio_path, "/direction");
+	
+	if ((fp = fopen("/sys/class/gpio/export", "ab")) == NULL)
+		return -1;
+	rewind(fp);	/*Set pointer to begining of the file*/
+	fwrite(pinN, sizeof(char), len_pinN, fp); /*Write number pin to the file*/
+	fclose(fp);	
+	status->export = 'y';
+	
+	if ((fp = fopen(gpio_path, "rb+")) == NULL) /*file in binary for reading and writing*/
+		return -1;
+	rewind(fp);	/*Set pointer to begining of the file*/
+	fwrite(dir, sizeof(char), len_dir, fp); /*Write "out" or "in" to the file*/
+	fclose(fp);
+
+	status->direction = *dir;
+	return 0;		
+}
+	
+int read_gpio_pin(status_gpio *status, const char *pinN)
+{
+	char gpio_value;
+	char gpio_path[30];
+	strcpy(gpio_path, "/sys/class/gpio/gpio");
+	strcat(gpio_path, pinN); 
+	strcat(gpio_path, "/value");
+		
+	if ((fp = fopen(gpio_path, "rb")) == NULL) /*file in binary for reading only*/
+		return -1;
+		
+	rewind(fp);	/*Set pointer to begining of the file*/
+	fread(&gpio_value, sizeof(char), 1, fp); /*Value read of the file*/
+	fclose(fp);
+	
+	status->value = gpio_value;
+	return 0;	
+}
+	
+int set_gpio_pin(status_gpio *status, const char *pinN)
+{
+	char gpio_path[30];
+	
+	if(status->direction == 'i') {printf("Error: GPIO input can't be set\n"); return -1;}
+	strcpy(gpio_path, "/sys/class/gpio/gpio");
+	strcat(gpio_path, pinN); 
+	strcat(gpio_path, "/value");
+		
+	if ((fp = fopen(gpio_path, "rb+")) == NULL) /*file in binary for reading and writing*/
+		return -1;
+		
+	rewind(fp);	/*Set pointer to begining of the file*/
+	fwrite("1", sizeof(char), 1, fp); /*Write "1" to the file*/
+	fclose(fp);
+	
+	status->value = '1';
+	return 0;
+}
+	
+int clear_gpio_pin(status_gpio *status, const char *pinN)
+{
+	char gpio_path[30];
+	
+	if(status->direction == 'i') {printf("Error: GPIO input can't be cleaned\n"); return -1;}
+	strcpy(gpio_path, "/sys/class/gpio/gpio");
+	strcat(gpio_path, pinN); 
+	strcat(gpio_path, "/value");
+		
+	if ((fp = fopen(gpio_path, "rb+")) == NULL) /*file in binary for reading and writing*/
+		return -1;
+		
+	rewind(fp);	/*Set pointer to begining of the file*/
+	fwrite("0", sizeof(char), 1, fp); /*Write "0" to the file*/
+	fclose(fp);	
+	
+	status->value = '0';
+	return 0;
+}	
---
+++ ant_RF/beagle/librfid-0.2.0_svn/rf2/gpio/gpio.h
@@ -0,0 +1,35 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+static const char OUT[] = "out";
+static const char IN[] = "in";
+
+typedef struct {
+
+		char export;
+		char direction;
+		char value;
+		char power;
+		char edge;
+		char uevent;
+}status_gpio;
+
+#define reset_status_gpio(status) \
+            status.export    = 'n'; \
+            status.direction = '0'; \
+            status.value     = '0'; \
+            status.power   	 = '0'; \
+            status.edge 	 = '0'; \
+            status.uevent  	 = '0'; 
+
+int config_gpio_pin(status_gpio *status, const char *dir, const char *pinN);
+int read_gpio_pin(status_gpio *status, const char *pinN);
+int set_gpio_pin(status_gpio *status, const char *pinN);
+int clear_gpio_pin(status_gpio *status, const char *pinN);
+
+#endif /*GPIO_H*/
