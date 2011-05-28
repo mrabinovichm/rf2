a2340900be66728efcebc5099879e07ba7caac31
edgardovaz@gmail.com <edgardovaz@gmail.com@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-05-28 17:04:15 +0000

    Se agregan librerías para el uso dentro de librfid.

---
+++ trunk/ant_RF/beagle/librfid-0.2.0_svn/rf2/gpio/beagle_gpio.h
@@ -0,0 +1,24 @@
+#ifndef BEAGLE_GPIO_H
+#define BEAGLE_GPIO_H
+
+/*EXPANSION CONNECTOR*/
+/*Pines usados como GPIO*/
+
+static const char PIN3[]  = "139";
+static const char PIN4[]  = "144";
+static const char PIN5[]  = "138";
+static const char PIN7[]  = "137";
+static const char PIN9[]  = "136";
+static const char PIN10[] = "145";
+static const char PIN12[] = "158";
+static const char PIN13[] = "134";
+static const char PIN14[] = "162";
+static const char PIN15[] = "133";
+static const char PIN16[] = "161";
+static const char PIN18[] = "159";
+static const char PIN20[] = "156";
+static const char PIN22[] = "157";
+static const char PIN23[] = "183";
+static const char PIN24[] = "168";
+
+#endif /*BEAGLE_GPIO_H*/
---
+++ trunk/ant_RF/beagle/librfid-0.2.0_svn/rf2/rf/rc632_utils.h
@@ -0,0 +1,8 @@
+#ifndef __RC632_UTILS_H__
+#define __RC632_UTILS_H__
+
+int init_rc632(void);
+int encender_rc632(void);
+int apagar_rc632(void);
+
+#endif/*__RC632_UTILS_H__*/
