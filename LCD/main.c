abc3e5f0e6b1d11725459b03d53d1f4a336d9ceb
mrabinovichm@gmail.com <mrabinovichm@gmail.com@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-06-04 21:23:03 +0000

    se definen mensajes para enviar al display

--- LCD/main.c
+++ LCD/main.c
@@ -3,7 +3,7 @@
 
 int main(void){
 
-	unsigned char es[] = "Esto funciona sin problemas, P3R";	/* esto se escribe al display */
+	//unsigned char es[] = "Esto funciona sin problemas, P3R";	/* esto se escribe al display */
 	
 	init_gpio();												/* inicialización de GPIO en beagleboard */
 	init_lcd();													/* inicialización del display 16x2 */
@@ -11,7 +11,7 @@ int main(void){
 	write_lcd(CLEAR, CTRL_WR);							 		/* por si el display ya tuviera algo escrito */
 	delay(2);
 	
-	dato_lcd(es, 32);
+	dato_lcd(prueba, 32);
 	delay(150000);												/* espera para poder leer el display */
 	
 	apagar();													/* borra y apaga el display */
