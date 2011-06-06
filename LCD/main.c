44ff05d7f5f53f2a0eec14cdb9e7a63031172887
mrabinovichm@gmail.com <mrabinovichm@gmail.com@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-06-06 22:35:15 +0000

    Se agregan varios mensajes en el main de prueba para chequear el funcionamiento.

--- LCD/main.c
+++ LCD/main.c
@@ -8,11 +8,29 @@ int main(void){
 	init_gpio();												/* inicialización de GPIO en beagleboard */
 	init_lcd();													/* inicialización del display 16x2 */
 	
-	write_lcd(CLEAR, CTRL_WR);							 		/* por si el display ya tuviera algo escrito */
-	delay(2);
+	//write_lcd(CLEAR, CTRL_WR);							 		/* por si el display ya tuviera algo escrito */
+	//delay(2);											estas 2 lineas fueron para adentro de dato_lcd
 	
-	dato_lcd(prueba, 32);
+	dato_lcd(prueba, sizeof(prueba));
 	delay(150000);												/* espera para poder leer el display */
+	
+	dato_lcd(inicio, sizeof(inicio));
+	delay(150000);					
+	
+	dato_lcd(tarjeta, sizeof(tarjeta));
+	delay(150000);					
+	
+	dato_lcd(quieto, sizeof(quieto));
+	delay(150000);					
+	
+	dato_lcd(err, sizeof(err));
+	delay(150000);					
+	
+	dato_lcd(esperar, sizeof(esperar));
+	delay(150000);					
+	
+	dato_lcd(saldoP, sizeof(saldoP));
+	delay(150000);					
 	
 	apagar_bl();
 	apagar_lcd();													/* borra y apaga el display */
