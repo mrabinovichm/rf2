9fdc3ec48c3c2c4e311e594550bd278d7762074e
mrabinovichm@gmail.com <mrabinovichm@gmail.com@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-06-15 21:01:19 +0000

    Se mejora el main de prueba, con varios mensajes escritos al display. Se crea macro en el "mensajes.h". Se modifica el Makefile, para incluir "mensajes.h".

--- LCD/mensajes.h
+++ LCD/mensajes.h
@@ -1,12 +1,15 @@
-
+#ifndef _MENSAJES_H_
+#define _MENSAJES_H_
 
 /* ****************************** Mensajes para el display *********************************************** */
 unsigned char prueba[] = "Esto funciona sin problemas, P3R";
-unsigned char inicio[] = "Prototipo de carga/consulta RF²";
-unsigned char tarjeta[] = "Aproxime su tarjeta.";
-unsigned char quieto[] = "Por favor, no retire su tarjeta.";
-unsigned char err[] = "Error, vuelva a intentarlo.";
-unsigned char esperar[] = "Espere un momento por favor.";
-unsigned char saldoV[] = "Su saldo en viajes es de"; //acá hay que hacer una concatenación de Strings para poner cuántos viajes
-unsigned char saldoP[] = "Su saldo en pesos es de";//acá hay que hacer una concatenación de Strings para poner cuaátos pesos
-unsigned char fin[] = "Transacción finalizada.";
+unsigned char inicio[] = " Prototipo RF2  carga/consulta";
+unsigned char tarjeta[] = "  Aproxime su   tarjeta";
+unsigned char quieto[] = " Por favor, no  retirar tarjeta";
+unsigned char err[] = "Error, vuelva a intentarlo";
+unsigned char esperar[] = "Espere por favor";
+unsigned char saldoV[] = "  Su saldo en   viajes es de"; //acá hay que hacer una concatenación de Strings para poner cuántos viajes
+unsigned char saldoP[] = "  Su saldo en   pesos es de";//acá hay que hacer una concatenación de Strings para poner cuaátos pesos
+unsigned char fin[] = "Transacción finalizada";
+
+#endif /*_MENSAJES_H_*/
