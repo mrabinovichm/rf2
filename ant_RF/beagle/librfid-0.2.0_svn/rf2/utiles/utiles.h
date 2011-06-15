3fbbcc3a0fcc1c5d55f213d7f48fac4f13f0a92b
daicav <daicav@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-06-15 16:52:35 +0000

    Se agregan utiles.c y utiles.h para el uso de funciones utiles del programa Se escriben las funciones "pot()" que calcula la potencia de un número a partir de su base y exponente (usar el de la librería math.h da problemas al compilar con gcc y no da para hacer grandes cambios por una función tan simple), "int_hex()" que convierte un entero (<256) en un byte en hexadecimal y "IntToBytePair()" que convierte un entero en 2 bytes hexadecimales

---
+++ ant_RF/beagle/librfid-0.2.0_svn/rf2/utiles/utiles.h
@@ -0,0 +1,14 @@
+#ifndef _INT_HEX_H_
+#define _INT_HEX_H_
+
+#include "tipo_datos.h"
+
+
+int pot(int base, int exp);
+
+void int_hexa(int entero, BYTE *hexadecimal);
+
+void IntToBytePair(int entero, BYTE *hex);
+
+
+#endif /*_INTHEX_H_*/
