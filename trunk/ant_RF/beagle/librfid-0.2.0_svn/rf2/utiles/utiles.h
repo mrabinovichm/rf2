4524fc8c1d2721dd09e8f02312004a2a558b209c
mrabinovichm@gmail.com <mrabinovichm@gmail.com@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-06-21 00:25:14 +0000

    En sam.c se agrega uid de otra tarjeta (led verde). En lcd16x2.c se eliminan comentarios innecesarios. En utiles.c: se soluciona bug en "concat_str_int()" cambiando un strcat() por un strcpy() y se agregan las funciones "leer_linea()" y "escribir_linea()" que leen y escriben respectivamente un archivo de texto alojado en la beagle para ver que monto cargar o no en las tarjetas, y se agrega "arrToInt()" para transformar un arreglo de caracteres en entero. La función "escribir_linea()" aun no se ha usado porque da problemas. En librfid-tool.c dentro de "consulta_recarga()" se incorpora el uso de las funciones mencionadas anteriormente.

--- trunk/ant_RF/beagle/librfid-0.2.0_svn/rf2/utiles/utiles.h
+++ trunk/ant_RF/beagle/librfid-0.2.0_svn/rf2/utiles/utiles.h
@@ -8,9 +8,14 @@ int pot(int base, int exp);
 
 void int_hexa(int entero, BYTE *hexadecimal);
 
+int arrToInt(char * buf, int largo_buf);
+
 void IntToBytePair(int entero, BYTE *hex);
 
 int concat_str_int(unsigned char *final, unsigned char *dato, int largo_dato, int entero);
 
+int leer_linea (int nro_lin, char *path);
+
+int escribir_linea (int nro_lin, char a, char *path);
 
 #endif /*_INTHEX_H_*/
