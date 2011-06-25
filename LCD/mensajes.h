cd4e6da6bf702d28071db3bf8e1b128604131eed
mrabinovichm@gmail.com <mrabinovichm@gmail.com@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-06-25 22:34:29 +0000

    Se eliminan comentarios innecesarios de ambos archivos.

--- LCD/mensajes.h
+++ LCD/mensajes.h
@@ -9,7 +9,7 @@ unsigned char quieto[] = " Por favor, no  retirar tarjeta";
 unsigned char err[] = "Error, vuelva a intentarlo";
 unsigned char esperar[] = "Espere por favor";
 unsigned char saldoV[] = "  Su saldo en   viajes es de"; //acá hay que hacer una concatenación de Strings para poner cuántos viajes
-unsigned char saldoP[] = "  Su saldo en   pesos es de";//acá hay que hacer una concatenación de Strings para poner cuaátos pesos
+unsigned char saldoP[] = "  Su saldo en   pesos es de"; //acá hay que hacer una concatenación de Strings para poner cuaátos pesos
 unsigned char fin[] = "Transacción finalizada";
 
 #endif /*_MENSAJES_H_*/
