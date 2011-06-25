213867592d021d81d9d8fa597001fb904058ab63
daicav <daicav@948cf517-5fb8-8a2b-d655-638daefefee0>
2011-06-25 20:57:14 +0000

    Se agregan comentarios a las funciones definidas

--- trunk/ant_RF/beagle/librfid-0.2.0_svn/rf2/rf/rc632_utils.h
+++ trunk/ant_RF/beagle/librfid-0.2.0_svn/rf2/rf/rc632_utils.h
@@ -1,8 +1,19 @@
 #ifndef __RC632_UTILS_H__
 #define __RC632_UTILS_H__
 
+// nombre:		init_rc632
+// descripción:	configura la pata rst_RF para poder usar el lector-escritor
+// @return		0: todo bien, -1: error
 int init_rc632(void);
+
+// nombre:		encender_rc632
+// descripción:	cambia el estado de rst_RF para habilitar el uso del lector-escritor
+// @return		0: todo bien, -1: error
 int encender_rc632(void);
+
+// nombre:		apagar_rc632
+// descripción:	cambia el estado de rst_RF para deshabilitar el uso del lector-escritor
+// @return		0: todo bien, -1: error
 int apagar_rc632(void);
 
 #endif/*__RC632_UTILS_H__*/
